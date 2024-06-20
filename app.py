45b4197508e106f3df1c080e25a46a680984a68a
kanchang12 <32091338+kanchang12@users.noreply.github.com>
2024-06-20 20:01:34 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,9 @@
 from flask import Flask, request, jsonify, render_template
 from roboflow import Roboflow
-import cv2
-import numpy as np
 import base64
-from flask_cors import CORS
 import os
 
 app = Flask(__name__)
-CORS(app)
-
 roboflow_api_key = os.getenv('ROBOFLOW_API_KEY')
 ROBOFLOW_WORKSPACE = "bird-v2"
 ROBOFLOW_VERSION = 2
@@ -23,15 +18,18 @@ def index():
 
 @app.route('/identify_bird', methods=['POST'])
 def identify_bird():
-    data = request.json
-    image_data = data.get('image_url').split(",")[1]
+    # Check if an image file is present in the request
+    if 'image' not in request.files:
+        return jsonify({"error": "No image file provided"}), 400
+    
+    image_file = request.files['image']
 
     try:
-        image = base64.b64decode(image_data)
-        image_array = np.frombuffer(image, dtype=np.uint8)
-        image = cv2.imdecode(image_array, cv2.IMREAD_COLOR)
-
-        result = model.predict(image, confidence=40, overlap=30).json()
+        # Read the image file
+        image_data = image_file.read()
+        
+        # Perform bird identification
+        result = model.predict(image_data, confidence=40, overlap=30).json()
 
         if result['predictions']:
             bird_name = result['predictions'][0]['class']
