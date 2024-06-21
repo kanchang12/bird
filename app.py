a1a931b972531583252a6288a662de4d4098044f
kanchang12 <32091338+kanchang12@users.noreply.github.com>
2024-06-21 04:50:07 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,15 @@
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, request, jsonify, render_template, make_response
 from roboflow import Roboflow
+import cv2
+import numpy as np
 import base64
 from flask_cors import CORS
 import os
 
 app = Flask(__name__)
-CORS(app) 
+CORS(app)
 
-
-roboflow_api_key = "282K9KJQbOG4dpF69t6D"
+roboflow_api_key = os.getenv('ROBOFLOW_API_KEY')
 ROBOFLOW_WORKSPACE = "bird-v2"
 ROBOFLOW_VERSION = 2
 
@@ -16,40 +17,38 @@ rf = Roboflow(api_key=roboflow_api_key)
 project = rf.workspace().project(ROBOFLOW_WORKSPACE)
 model = project.version(ROBOFLOW_VERSION).model
 
-
-@app.before_request
-def before_request():
-    request.headers['Permissions-Policy'] = 'geolocation=(self), camera=(), microphone=(), fullscreen=*, payment=()'
-
+def add_permissions_policy_headers(response):
+    response.headers['Permissions-Policy'] = 'geolocation=(self), camera=(), microphone=(), fullscreen=*, payment=()'
+    return response
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    response = make_response(render_template('index.html'))
+    return add_permissions_policy_headers(response)
 
 @app.route('/identify_bird', methods=['POST'])
 def identify_bird():
-    # Check if an image file is present in the request
-    if 'image' not in request.files:
-        return jsonify({"error": "No image file provided"}), 400
-    
-    image_file = request.files['image']
+    data = request.json
+    image_data = data.get('image_url').split(",")[1]
 
     try:
-        # Read the image file
-        image_data = image_file.read()
-        
-        # Perform bird identification
-        result = model.predict(image_data, confidence=40, overlap=30).json()
+        image = base64.b64decode(image_data)
+        image_array = np.frombuffer(image, dtype=np.uint8)
+        image = cv2.imdecode(image_array, cv2.IMREAD_COLOR)
+
+        result = model.predict(image, confidence=40, overlap=30).json()
 
         if result['predictions']:
             bird_name = result['predictions'][0]['class']
             print(bird_name)
-            return jsonify({"bird_name": bird_name})
+            response = jsonify({"bird_name": bird_name})
         else:
-            return jsonify({"bird_name": "No bird detected"})
+            response = jsonify({"bird_name": "No bird detected"})
 
     except Exception as e:
-        return jsonify({"error": str(e)}), 500
+        response = jsonify({"error": str(e)}), 500
+
+    return add_permissions_policy_headers(response)
 
 if __name__ == '__main__':
     app.run(debug=True)
