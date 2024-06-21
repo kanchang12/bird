7409b978f32ba9441f2eb2ce4a5881345991a957
kanchang12 <32091338+kanchang12@users.noreply.github.com>
2024-06-21 06:13:52 +0100

    Update app.py

--- app.py
+++ app.py
@@ -32,17 +32,20 @@ def identify_bird():
     try:
         data = request.json
         if not data or 'image_url' not in data:
-            print("No image data received")
-            return jsonify({"error": "No image data received"}), 400
+            print("No image URL received")
+            return jsonify({"error": "No image URL received"}), 400
 
-        image_data = data.get('image_url').split(",")[1]
-        print("Image data length:", len(image_data))
+        image_url = data.get('image_url')
+        print("Image URL:", image_url)
 
-        image = base64.b64decode(image_data)
-        image_array = np.frombuffer(image, dtype=np.uint8)
-        image = cv2.imdecode(image_array, cv2.IMREAD_COLOR)
+        # Download the image
+        response = requests.get(image_url)
+        image = Image.open(BytesIO(response.content))
 
-        result = model.predict(image, confidence=40, overlap=30).json()
+        # Convert PIL Image to numpy array
+        image_np = np.array(image)
+
+        result = model.predict(image_np, confidence=40, overlap=30).json()
         print("Received result from model:", result)
 
         if result['predictions']:
